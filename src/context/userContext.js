dbd7149d9e94c4b7e280492f1028ce5a39004b82
pebblepark <subling02@gmail.com>
2021-09-08 00:19:53 +0900

    API 처리 함수 만들기

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { createContext, useContext, useReducer } from 'react';
 
 const initialState = {
@@ -99,4 +100,28 @@ export function useUsersDispatch() {
   }
 
   return dispatch;
+}
+
+export async function getUsers(dispatch) {
+  dispatch({ type: 'GET_USERS' });
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    dispatch({ type: 'GET_USERS_SUCCESS', data: response.data });
+  } catch (e) {
+    dispatch({ type: 'GET_USERS_ERROR', error: e });
+  }
+}
+
+export async function getUser(dispatch, id) {
+  dispatch({ type: 'GET_USER' });
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+    );
+    dispatch({ type: 'GET_USER_SUCCESS', data: response.data });
+  } catch (e) {
+    dispatch({ type: 'GET_USER_ERROR', error: e });
+  }
 }
