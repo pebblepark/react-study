405718bd137e256f1ae7e3e09a7328f516e17629
pebblepark <subling02@gmail.com>
2021-09-14 10:11:00 +0900

    리덕스 개발자도구 적용

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
