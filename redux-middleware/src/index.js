be142534bd6ef1e0a895dc14e57e85993cf000ec
pebblepark <subling02@gmail.com>
2021-10-12 21:11:14 +0900

    redux-logger 및 redux-devtools 추가

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -6,9 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
-import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
