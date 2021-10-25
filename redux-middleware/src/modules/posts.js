29118ec3d8c032f2942ceea26af9438cce0b69ff
pebblepark <subling02@gmail.com>
2021-10-26 00:00:35 +0900

    saga에 라우터 연동

--- redux-middleware/src/modules/posts.js
+++ redux-middleware/src/modules/posts.js
@@ -6,7 +6,7 @@ import {
   createPromiseSaga,
   createPromiseSagaById,
 } from "../lib/asyncUtils";
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, getContext } from "redux-saga/effects";
 
 const GET_POSTS = "GET_POSTS";
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
@@ -16,8 +16,15 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
+const GO_TO_HOME = "GO_TO_HOME";
+
 export const getPosts = () => ({ type: GET_POSTS });
 export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id });
+
+function* goToHomeSaga() {
+  const history = yield getContext("history");
+  history.push("/");
+}
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postAPI.getPostById);
@@ -25,13 +32,10 @@ const getPostSaga = createPromiseSagaById(GET_POST, postAPI.getPostById);
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
 
-export const goToHome =
-  () =>
-  (dispatch, getState, { history }) => {
-    history.push("/");
-  };
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 const initialState = {
   posts: reducerUtils.initial(),
