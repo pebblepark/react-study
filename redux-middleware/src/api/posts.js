97fa1a1a1bef50150edb1dfa4858ec5ff2b72b11
pebblepark <subling02@gmail.com>
2021-10-25 22:32:41 +0900

    json-server용 data.json 파일 및 axios 라이브러리 추가

--- redux-middleware/src/api/posts.js
+++ redux-middleware/src/api/posts.js
@@ -1,30 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-// { id, title, body }
-const posts = [
-  {
-    id: 1,
-    title: "리덕스 미들웨어를 배워봅시다.",
-    body: "리덕스 미들웨어를 직접 만들어보면 이해하기가 쉽죠.",
-  },
-  {
-    id: 2,
-    title: "redux-thunk를 사용해봅시다..",
-    body: "redux-thunk를 사용해서 비동기 작업을 처리해보아요.",
-  },
-  {
-    id: 3,
-    title: "redux-saga도 사용해봅시다.",
-    body: "나중에 redux-saga를 사용해서 비동기 작업을 처리하는 방법도 배워볼거에요.",
-  },
-];
+import axios from "axios";
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get("http://localhost:4000/posts");
+  return response.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 };
