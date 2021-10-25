5207a8b61bfc56474d40d30e76cba57f1e4a5c58
pebblepark <subling02@gmail.com>
2021-10-25 23:41:51 +0900

    프로미스 다루기

--- redux-middleware/src/modules/posts.js
+++ redux-middleware/src/modules/posts.js
@@ -6,6 +6,7 @@ import {
   handleAsyncActionsById,
   reducerUtils,
 } from "../lib/asyncUtils";
+import { call, put, takeEvery } from "redux-saga/effects";
 
 const GET_POSTS = "GET_POSTS";
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
@@ -15,8 +16,49 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
-export const getPosts = createPromiseThunk(GET_POSTS, postAPI.getPosts);
-export const getPost = ceratePromiseThunkById(GET_POST, postAPI.getPostById);
+export const getPosts = () => ({ type: GET_POSTS });
+export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id });
+
+function* getPostsSaga() {
+  try {
+    const posts = yield call(postAPI.getPosts);
+    yield put({
+      type: GET_POSTS_SUCCESS,
+      payload: posts,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POSTS_ERROR,
+      payload: e,
+      error: true,
+    });
+  }
+}
+
+function* getPostSaga(action) {
+  const id = action.payload;
+  try {
+    const post = yield call(postAPI.getPostById, id);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post,
+      meta: id,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_ERROR,
+      payload: e,
+      error: true,
+      meta: id,
+    });
+  }
+}
+
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
+
 export const goToHome =
   () =>
   (dispatch, getState, { history }) => {
