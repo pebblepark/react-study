b289f256622d30b8e2060b654bc1223ea987dcf4
pebblepark <subling02@gmail.com>
2022-02-11 23:29:48 +0900

    test: sumOf 함수 테스트 추가

--- react-tdd/src/sum.test.js
+++ react-tdd/src/sum.test.js
@@ -1,5 +1,12 @@
-const sum = require('./sum');
+const { sum, sumOf } = require('./sum');
 
-it('calculates 1 + 2', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('sum', () => {
+  it('calculates 1 + 2', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it('calculates all numbers', () => {
+    const array = [1, 2, 3, 4, 5];
+    expect(sumOf(array)).toBe(15);
+  });
 });
