0b90001f1831f8d39b9dbe7e00c8f2bb4fb563c4
pebblepark <subling02@gmail.com>
2022-02-12 23:30:56 +0900

    refactor: Math.max 함수 사용

--- react-tdd/src/practice/stats.js
+++ react-tdd/src/practice/stats.js
@@ -1,9 +1,1 @@
-exports.max = (numbers) => {
-  let result = numbers[0];
-  numbers.forEach((n) => {
-    if (n > result) {
-      result = n;
-    }
-  });
-  return result;
-};
+exports.max = (numbers) => Math.max(...numbers);
