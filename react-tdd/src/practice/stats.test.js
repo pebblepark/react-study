261f85d4affbce3164162810826e6cf39eab66ae
pebblepark <subling02@gmail.com>
2022-02-13 13:52:11 +0900

    test: 평균값 구하기 테스트 케이스 작성

--- react-tdd/src/practice/stats.test.js
+++ react-tdd/src/practice/stats.test.js
@@ -7,4 +7,7 @@ describe('stats', () => {
   it('gets minimum value', () => {
     expect(stats.min([1, 2, 3, 4])).toBe(1);
   });
+  it('gets average value', () => {
+    expect(stats.avg([1, 2, 3, 4, 5])).toBe(3);
+  });
 });
