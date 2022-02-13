4cd4e8917a973608990056a00c7a6e1b58155a09
pebblepark <subling02@gmail.com>
2022-02-13 13:47:26 +0900

    test: 최솟값 구하기 테스트 케이스 작성

--- react-tdd/src/practice/stats.test.js
+++ react-tdd/src/practice/stats.test.js
@@ -4,4 +4,7 @@ describe('stats', () => {
   it('gets maximum value', () => {
     expect(stats.max([1, 2, 3, 4])).toBe(4);
   });
+  it('gets minimum value', () => {
+    expect(stats.min([1, 2, 3, 4])).toBe(1);
+  });
 });
