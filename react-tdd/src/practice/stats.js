a5e22e6f4c95df08023fa89aecb3b25e920848cd
pebblepark <subling02@gmail.com>
2022-02-13 14:07:15 +0900

    feat: 중앙값 구하기 함수 작성

--- react-tdd/src/practice/stats.js
+++ react-tdd/src/practice/stats.js
@@ -2,3 +2,12 @@ exports.max = (numbers) => Math.max(...numbers);
 exports.min = (numbers) => Math.min(...numbers);
 exports.avg = (numbers) =>
   numbers.reduce((acc, curr, _, { length }) => acc + curr / length, 0);
+exports.sort = (numbers) => numbers.sort((a, b) => a - b);
+exports.median = (numbers) => {
+  numbers = exports.sort(numbers);
+  const { length } = numbers;
+  const middle = Math.floor(length / 2);
+  return length % 2
+    ? numbers[middle]
+    : (numbers[middle - 1] + numbers[middle]) / 2;
+};
