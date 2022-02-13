2743b95c2526e1dc056f9412f868b6feff7775b5
pebblepark <subling02@gmail.com>
2022-02-13 13:52:26 +0900

    feat: 평균값 구하기 함수 작성

--- react-tdd/src/practice/stats.js
+++ react-tdd/src/practice/stats.js
@@ -1,2 +1,6 @@
 exports.max = (numbers) => Math.max(...numbers);
 exports.min = (numbers) => Math.min(...numbers);
+exports.avg = (numbers) => {
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return sum / numbers.length;
+};
