ebfd7341d8b1724d3e6cb71095071fe5e821d565
pebblepark <subling02@gmail.com>
2022-02-16 16:16:16 +0900

    feat: Post 조회 예제 추가

--- react-query/src/App.tsx
+++ react-query/src/App.tsx
@@ -1,5 +1,31 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const response = await axios.get("http://localhost:3000/posts");
+  return response.data;
+};
+
 function App() {
-  return <div>hello</div>;
+  const { data, isLoading, error } = useQuery<Post[]>("posts", fetchPosts);
+
+  if (isLoading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생: {error}</div>;
+
+  return (
+    <div>
+      {data?.map((post: Post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
